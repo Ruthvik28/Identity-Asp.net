56c188c659c07aca351529653478e443ecb078bb
Ruthvik Suvarnakanti <ruthviksuvarnakanti@Ruthviks-Air.lan>
2024-11-13 21:46:43 -0500

    first commit

---
+++ Asp_Identity/Pages/Login.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp_Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Asp_Identity.Pages
+{
+	public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Login LogModel { get; set; }
+
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public void OnGet()
+        {
+        }
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var result= await signInManager.PasswordSignInAsync(LogModel.Email, LogModel.Password,false,false);
+                if (result.Succeeded)
+                {
+                   
+                        return RedirectToPage("Index");
+                
+                    
+                    
+                }
+                ModelState.AddModelError("", "Incorrect Email or Password");
+            }
+            return Page();
+        }
+
+       
+    }
+}
---
+++ Asp_Identity/Pages/Register.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp_Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Asp_Identity.Pages
+{
+	public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> usermanager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [BindProperty]
+        public Register RegModel { get; set; }
+
+
+        public RegisterModel(UserManager<IdentityUser> usermanager, SignInManager<IdentityUser> signInManager)
+        {
+            this.usermanager = usermanager;
+            this.signInManager = signInManager;
+        }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = RegModel.Email,
+                    Email = RegModel.Email
+                };
+               var result = await usermanager.CreateAsync(user, RegModel.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+
+            return Page();
+            
+            
+        }
+    }
+}
